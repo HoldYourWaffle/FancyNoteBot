0f4854a31b7f8333ac4965a230a6573c2c29046b
HoldYourWaffle <ravivanrooijen@live.nl>
2017-08-04 14:16:21 +0200

    Moving cross-compiling tools in seperate directory

--- jre-downloader.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p "build/tmp/jre"
-mkdir "windows-jre"
-cd "build/tmp/jre"
-wget "https://github.com/ojdkbuild/ojdkbuild/releases/download/1.8.0.141-1/java-1.8.0-openjdk-1.8.0.141-1.b16.ojdkbuild.windows.x86_64.zip" -O "jre.zip"
-unzip "jre.zip"
-mv java-* extract/
-mv extract/jre/ ../../../windows-jre/
