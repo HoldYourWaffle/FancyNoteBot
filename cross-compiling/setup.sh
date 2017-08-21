e2da48fdad95f25e7a795d893888abf10463a608
HoldYourWaffle <ravivanrooijen@live.nl>
2017-08-21 11:52:19 +0200

    Removing useless GET parameters

--- cross-compiling/setup.sh
+++ cross-compiling/setup.sh
@@ -8,7 +8,7 @@ cd $scriptdir # change into cross-compiling directory
 echo "Downloading Launch4j..."
 rm -fr "launch4j" # with the -f flag it won't fail when there is no "launch4j" folder (AKA no launch4j-dist has ever been downloaded)
 mkdir -p "../build/tmp/launch4j" && cd "$_" # create temporary extraction folder in build directory. This way it'll get cleaned up automatically with gradle clean
-wget "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.11/launch4j-3.11-linux-x64.tgz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Flaunch4j%2Ffiles%2Flaunch4j-3%2F3.11%2F&ts=1501782812&use_mirror=netcologne" -nv -O "launch4j.tgz"
+wget "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.11/launch4j-3.11-linux-x64.tgz" -nv -O "launch4j.tgz"
 echo "Extracting Launch4j..."
 tar xf "launch4j.tgz"
 mv "launch4j/" "$scriptdir/" # move extracted launch4j distro into the cross-compiling directory
